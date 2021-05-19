1c46b241045a08eb1db724d1acb0601c7f457e7d
DimaSilenko <rotor12587@mail.ru>
2021-05-19 22:17:42 +0300

    First step

---
+++ src/main/java/com/example/project/controller/UsersController.java
@@ -0,0 +1,34 @@
+package com.example.project.controller;
+
+import com.example.project.entity.Users;
+import com.example.project.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UsersController {
+
+    @Autowired
+    UsersRepository usersRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        Iterable<Users> usersIt = usersRepository.findAll();
+        model.addAttribute("usersIt", usersIt);
+        model.addAttribute("usersModel", new Users());
+        return "index";
+    }
+
+    @PostMapping("/submit-users")
+    public String index(Model model, Users users) {
+        usersRepository.save(users);
+        Iterable<Users> usersIt = usersRepository.findAll();
+        model.addAttribute("usersIt", usersIt);
+        return "result";
+        //return index(model);
+    }
+}
---
+++ src/main/java/com/example/project/entity/Users.java
@@ -0,0 +1,33 @@
+package com.example.project.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public Users() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
